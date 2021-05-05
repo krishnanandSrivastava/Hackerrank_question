3d8157230e379063a9f4ba86b929eb2ecf9ec713
KRISHNANAND <krishnanand.email@gmail.com>
2021-05-05 19:26:14 +0530

    Create Matching Digits & Non-Digit Characters.java
    The expression \d matches any digit [ - ].
    The expression \D matches any character that is not a digit.
    
    Task
    
    You have a test string . Your task is to match the pattern 
    Here  denotes a digit character, and  denotes a non-digit character.
    
    Note
    
    This is a regex only challenge. You are not required to write any code.
    You only have to fill the regex pattern in the blank (_________).

---
+++ Matching Digits & Non-Digit Characters.java
@@ -0,0 +1,12 @@
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(\\d\\d\\D){2}\\d\\d\\d\\d"); // Use \\ instead of using \ 
+    
+    }
+}
+
