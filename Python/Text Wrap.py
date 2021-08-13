427ae38214e36fa7bbe6a210987d39d79d1b9c67
KRISHNANAND <krishnanand.email@gmail.com>
2021-08-13 19:52:47 +0530

    Create Text Wrap.py
    Check Tutorial tab to know how to to solve.
    
    You are given a string  and width .
    Your task is to wrap the string into a paragraph of width .
    
    Function Description
    
    Complete the wrap function in the editor below.
    
    wrap has the following parameters:
    
    string string: a long string
    int max_width: the width to wrap to
    Returns
    
    string: a single string with newline characters ('\n') where the breaks should be
    Input Format
    
    The first line contains a string, .
    The second line contains the width, .
    
    Constraints
    
    Sample Input 0
    
    ABCDEFGHIJKLIMNOQRSTUVWXYZ
    4
    Sample Output 0
    
    ABCD
    EFGH
    IJKL
    IMNO
    QRST
    UVWX
    YZ

---
+++ Python/Text Wrap.py
@@ -0,0 +1,8 @@
+import textwrap
+
+def wrap(string, max_width):
+    s=""
+    for i in range(0,len(string),max_width):
+        s+=string[i:i+max_width]+"\n"
+    return s
+if __name__ == '__main__':
